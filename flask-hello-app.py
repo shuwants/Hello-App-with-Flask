bf8c44464e17564eb7c697c009d6288300d3247a
Shuji Kato <shuwants@gmail.com>
2020-12-10 16:17:07 +0900

    First commit

---
+++ flask-hello-app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/localhost:3000')
+def index():
+    return 'Hello World!'
+
+# With the lines below, we can run server by only "python3 app.py".
+if __name__ == '__main__':
+    app.run(
+      port=3000,
+      debug=True,
+      host='127.0.0.1'
+      )
